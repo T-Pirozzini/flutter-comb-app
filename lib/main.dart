b70f18bf39d0d0311db20b77ab737654810f6f5b
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2023-01-04 11:53:54 -0800

    implement google maps package

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,45 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.amber,
         appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
+          title: Text(
+            "Comb",
+            style: TextStyle(
+                fontSize: 32.0,
+                fontFamily: 'Alice',
+                color: Color.fromARGB(255, 218, 69, 99)),
+          ),
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 5, 32, 45),
+          elevation: 0.0,
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              const DrawerHeader(
+                child: Text('Welcome to Comb...'),
+              ),
+              ListTile(
+                title: const Text("Login"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("Register"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("About"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
         ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
@@ -33,6 +69,22 @@ class _MyAppState extends State<MyApp> {
             target: _center,
             zoom: 11.0,
           ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              label: 'Harvest Locations',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat_bubble),
+              label: 'Message Board',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.info),
+              label: 'Species Info',
+            ),
+          ],
         ),
       ),
     );
